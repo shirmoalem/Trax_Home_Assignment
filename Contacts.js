008124603b19c65eb87d0f18b6067362fef9e286
shirmoalem <63964216+shirmoalem@users.noreply.github.com>
2020-10-28 15:20:53 +0200

    init

---
+++ Contacts.js
@@ -0,0 +1,9 @@
+let contacts =
+    [
+    { fullName: 'Shir Moalem', email: 'shirmoalem51@gmail.com',  phone: '054-7664451' },
+    { fullName: 'Tal Cohen', email: 'talcohen@gmail.com',  phone: '054-1231133' },
+    { fullName: 'Liav Bar', email: 'liavbar@gmail.com',  phone: '054-5356643' },
+    { fullName: 'Dan Rozen', email: 'danrozen@gmail.com',  phone: '052-5675432' }
+    ];
+
+module.exports  = contacts;
